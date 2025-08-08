e92af540da1126fd3d2b0493f2474141ad950837
serge <125203998+serge740@users.noreply.github.com>
2025-08-08 04:07:57 +0300

    finished project page

--- src/pages/Projects/ProjectPages.jsx
+++ src/pages/Projects/ProjectPages.jsx
@@ -165,7 +165,7 @@ const ProjectsPage = () => {
             </div>
 
             {/* Projects Grid */}
-            <div className=" w-10/12 mx-auto px-6">
+            <div className=" w-full md:w-10/12 mx-auto px-6">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {projects.map((project) => (
                         <ProjectCard key={project.id} project={project} />
